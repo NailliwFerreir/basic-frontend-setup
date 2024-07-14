b8486998b6f4acb0fd81eb348a4f9fe9504ec9dc
NailliwFerreir <williansferreir@icloud.com>
2024-07-14 17:44:10 -0300

    chore: adding helmet custom for home

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { Helmet } from 'react-helmet-async'
 
 export function Home() {
   return (
-    <div className="w-full h-screen items-center justify-center bg-gray-300 p-4">
-      <Button>Home!</Button>
-    </div>
+    <>
+      <Helmet title='Home'/>
+      <div className="w-full h-screen items-center justify-center bg-gray-300 p-4">
+        <Button>Home!</Button>
+      </div>
+    </>
   )
 }
