9e9557af6ea47007f1c1832c4cc4a21525a1bece
NailliwFerreir <williansferreir@icloud.com>
2024-07-14 17:41:39 -0300

    chore: adding helmet custom for app

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { queryClient } from '@/lib/query.ts'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/query.ts'
 import { RouterProvider } from 'react-router'
-import { router } from './router.tsx'
 import './global.css'
+import { router } from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+    <HelmetProvider>
+      <Helmet titleTemplate='%s | basic.' />
       <RouterProvider router={router} />
+    </HelmetProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
